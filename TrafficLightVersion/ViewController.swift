f47924da187d10eb838f475395a5c783ad9029ae
Kirill Ki <kirillkalapov@gmail.com>
2021-02-09 15:46:28 +0100

    New version of The Light project. This app switch red, green and yellow color like traffic light

--- TrafficLightVersion/ViewController.swift
+++ TrafficLightVersion/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isLightOn = 0
+    
+    
+    
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        
     }
-
-
+    
+    /// This function changing screen color
+    fileprivate func updateUI() {
+        
+        switch isLightOn {
+        case 1:
+            view.backgroundColor = .green
+        case 2:
+            view.backgroundColor = .yellow
+        case 3:
+            view.backgroundColor = .red
+        default:
+            view.backgroundColor = .black
+        }
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isLightOn = isLightOn < 3 ? isLightOn + 1 : 1
+        updateUI()
+        
+    }
+    
+    
+    
+    
 }
+
 
